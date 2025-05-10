7fe6337cf0701b073d07e9548f1f515e69d1cac0
zoebchhatriwala <zrex.mail@gmail.com>
2025-05-10 21:15:47 +0530

    Improve email obfuscation

--- data_obfuscator/obfuscator.py
+++ data_obfuscator/obfuscator.py
@@ -12,39 +12,42 @@ class DataObfuscator:
         # Mapping: document_id -> { names, amounts, emails }
         self.data_mappings = defaultdict(lambda: {"names": {}, "amounts": {}, "emails": {}})
 
-    def _generate_unique_organization_name(self, used_names, length=16):
+    def generate_random_str(self, length=16):
         alphabets = 'abcdefghijklmnopqrstuvwxyz'
-        name = ''.join(random.choices(alphabets, k=length)).capitalize()
+        return ''.join(random.choices(alphabets, k=length))
+    
+    def is_valid_email(self, email: str):
+        # Simple regex for email validation
+        pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b'
+        return re.match(pattern, email) is not None
+
+    def _generate_unique_organization_name(self, used_names: set, length=16):
+        name = self.generate_random_str(length).capitalize()
         while True:
             if name not in used_names:
                 return name
 
-    def _generate_unique_random_name(self, used_names):
-        alphabets = 'abcdefghijklmnopqrstuvwxyz'
-        first_name = ''.join(random.choices(alphabets, k=8)).capitalize()
-        last_name = ''.join(random.choices(alphabets, k=8)).capitalize()
+    def _generate_unique_random_name(self, used_names: set):
+        first_name = self.generate_random_str(8).capitalize()
+        last_name = self.generate_random_str(8).capitalize()
         while True:
             name = f"{first_name} {last_name}"
             if name not in used_names:
                 return name
 
-    def _generate_unique_random_amount(self, used_amounts):
+    def _generate_unique_random_amount(self, used_amounts: set):
         while True:
             amount = f"{random.randint(1000, 10000)}.{random.randint(0, 999999):06d}"
             if amount not in used_amounts:
                 return amount
     
-    def is_valid_email(self, email):
-        # Simple regex for email validation
-        pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b'
-        return re.match(pattern, email) is not None
-    
-     # Handle email obfuscation
-    def hash_email(self, email):
-        hashed = hashlib.sha256(email.encode()).hexdigest()[:10] + "@example.com"
-        return hashed
+    def _generate_unique_random_email(self, email: str):
+        [address, domain] = email.split('@')
+        [local_part, domain] = domain.split('.')
+        email = self.generate_random_str(len(address)) + "@" + self.generate_random_str(length=len(local_part)) + "." + self.generate_random_str(length=len(domain))
+        return email
 
-    def obfuscate(self, text, document_id="default_doc"):
+    def obfuscate(self, text: str, document_id="default_doc"):
         doc = nlp(text)
         replacements = []
 
@@ -70,7 +73,7 @@ class DataObfuscator:
                         replacement = emails_map[original]
                     else:
                         # If not, hash the email
-                        replacement = self.hash_email(original)
+                        replacement = self._generate_unique_random_email(original)
 
                     # Store the mapping
                     emails_map[original] = replacement
@@ -139,7 +142,7 @@ class DataObfuscator:
                     replacement = emails_map[original]
                 else:
                     # If not, hash the email
-                    replacement = self.hash_email(original)
+                    replacement = self._generate_unique_random_email(original)
 
                     # Store the mapping
                     emails_map[original] = replacement
@@ -161,7 +164,7 @@ class DataObfuscator:
         # Return the redacted text
         return redacted_text
 
-    def restore(self, obfuscated_text, document_id="default_doc"):
+    def restore(self, obfuscated_text: str, document_id="default_doc"):
         # Initialize the restored text
         restored_text = obfuscated_text
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data_obfuscator import DataObfuscator
 obfuscator = DataObfuscator()
 
 text = """
-Agreement between John Doe (john.doe@example.com) and John Doe.
+Agreement between John Doe (john.doe@example.com) and Jane Doe.
 Payment: $7000.00 due on 2025-12-01.
 Contact: john.doe@example.com
 Company: Acme Corp.
